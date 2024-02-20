8d9a5f4739ac24e5a3e8c6216d16ddcb5c9324e3
Dineshpokhrel <dineshpokhrel001@gmail.com>
2024-02-20 12:55:40 +0545

    understanding utils codes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log (`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failes !!!", err);
+})
 
 
 
